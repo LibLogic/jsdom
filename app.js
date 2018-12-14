037863113c726835bb5e81215279be52cffaafec
LibLogic <hdgknsn@gmail.com>
2018-12-14 14:37:43 +0000

    experiments in app.js — appending to DOM elements

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const wmf = document.querySelector("#book-list li:nth-child(2) .name");
-//console.log(wmf);
+var books = document.querySelectorAll("#book-list li .name");
 
-var books = document.querySelector("#book-list li .name");
-//console.log(books);
+books.forEach(function(book){
+    book.textContent += " (book title)";
+})
 
-books = document.querySelectorAll("#book-list li .name");
-console.log(books);
+const bookList = document.querySelector("#book-list");
+bookList.innerHTML += '<p>This is how you add HTML</p>';
 
 //https://www.youtube.com/watch?v=FIORjGvT0kk&list=PL4cUxeGkcC9gfoKa5la9dsdCNpuey2s-V&index=1
-books.forEach(function(book){
-    console.log(book);
-})
