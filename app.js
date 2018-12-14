a2e9ea4371d8dc4bb17a026c69d0714108bf6760
LibLogic <hdgknsn@gmail.com>
2018-12-14 19:17:02 +0000

    experimenting with DOM traversal. nextSibling, nextElementSibling, previousSibling and previousElementSibling — video 8

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const bookList = document.querySelector('#book-list')
 
-console.log('The parent node is',bookList.parentNode);
+console.log('The next sibling is"', bookList.nextSibling);
+console.log('The next sibling is"', bookList.nextElementSibling);
 
-console.log('The parent node is',bookList.parentElement.parentElement);
+console.log('The previous sibling is"', bookList.previousSibling);
+console.log('The previous sibling is"', bookList.previousElementSibling);
 
-console.log(bookList.childNodes);
-
-console.log(bookList.children);
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br>Too cool for everyone else!';
 
 //https://www.youtube.com/watch?v=FIORjGvT0kk&list=PL4cUxeGkcC9gfoKa5la9dsdCNpuey2s-V&index=1
