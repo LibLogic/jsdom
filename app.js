12b30a4063ef4468e05646a3fc330911959a4643
LibLogic <hdgknsn@gmail.com>
2018-12-16 01:26:22 +0000

    Changed app.js and experimented with creating and appending elements

--- app.js
+++ app.js
@@ -8,13 +8,30 @@ list.addEventListener('click', function(e){
 })
 
 //add books
-
 const addForm = document.forms['add-book'];
 
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+
+    //Create needed elements
+    var li = document.createElement('li');
+    var bookName = document.createElement('span');
+    var deleteBtn = document.createElement('span');
+    
+    //Add content to elements
+    bookName.textContent = value;
+    deleteBtn.textContent = 'delete'
+    
+    //Create DOM structure and append it to the UL
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 })
+
+
+
+
+
 
 //https://www.youtube.com/watch?v=FIORjGvT0kk&list=PL4cUxeGkcC9gfoKa5la9dsdCNpuey2s-V&index=1
